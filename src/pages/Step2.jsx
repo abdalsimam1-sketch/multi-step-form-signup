d385e95a69dd43f673cede63cab6451401a1fa1b
abdalsimam1-sketch <abdalsimam1@gmail.com>
2025-12-27 22:01:51 +0100

    third

--- src/pages/Step2.jsx
+++ src/pages/Step2.jsx
@@ -27,14 +27,14 @@ export const Step2 = () => {
   return (
     <>
       <div
-        className="row justify-content-center align-items-center m-5 p-5 shadow gap-3 col-12 mx-auto col-md-12  col-lg-8  "
+        className="row justify-content-center align-items-stretch m-5 p-5 shadow gap-3 col-12 mx-auto col-lg-8"
         style={{ borderRadius: "1rem" }}
       >
         <div
-          className="col-4 left-side  shadow p-5 text-capitalize"
+          className="col-12 col-md-4 left-side  shadow p-5 text-capitalize d-flex justify-content-between d-md-inline"
           style={{
             backgroundSize: "cover",
-            height: "80vh",
+
             fontSize: "0.9rem",
           }}
         >
@@ -42,7 +42,7 @@ export const Step2 = () => {
             <div className="col-2">
               <i className="bi bi-1-circle fs-3"></i>
             </div>
-            <div className="col-8  ms-2">
+            <div className="col-8  ms-2 d-none d-md-inline">
               <p className="m-0 fw-bold">step 1</p>
               <span className="text-muted">your info </span>
             </div>
@@ -52,7 +52,7 @@ export const Step2 = () => {
               <i className="bi bi-2-circle-fill fs-3"></i>
             </div>
 
-            <div className="col-8 ms-2">
+            <div className="col-8 ms-2  d-none d-md-inline">
               <p className="m-0 fw-bold">step 2</p>
               <span>select plan</span>
             </div>
@@ -60,9 +60,9 @@ export const Step2 = () => {
           <div className="row mb-3">
             <div className="col-2">
               {" "}
-              <i className="bi bi-3-circle fs-3"></i>
+              <i className="bi bi-3-circle fs-3 "></i>
             </div>
-            <div className="col-8 ms-2">
+            <div className="col-8 ms-2  d-none d-md-inline">
               <p className="m-0 fw-bold">step 3</p>
               <span>add-ons</span>
             </div>
@@ -71,14 +71,14 @@ export const Step2 = () => {
             <div className="col-2">
               <i className="bi bi-4-circle fs-3"></i>
             </div>
-            <div className="col-8 ms-2">
+            <div className="col-8 ms-2  d-none d-md-inline">
               <p className="fw-bold m-0">step 4</p>
               <span>summary</span>
             </div>
           </div>
         </div>
         <div
-          className="col-7 p-5 text-capitalize shadow position-relative"
+          className="col-12 col-md-7 p-5 text-capitalize shadow position-relative"
           style={{
             backgroundSize: "cover",
             height: "80vh",
@@ -91,7 +91,7 @@ export const Step2 = () => {
             <p className="text-muted">
               you have the option of monthly or yearly billing
             </p>
-            <div className="row mt-5 gap-3 justify-content-center position-relative p-2 ">
+            <div className="row mt-3 mt-md-5 gap-3  position-relative p-4 d-flex flex-column align-items-center flex-md-row">
               <div
                 onClick={() => {
                   setFormData({
@@ -100,7 +100,7 @@ export const Step2 = () => {
                     planPrice: planPrices["arcade"],
                   });
                 }}
-                className="col-3 plan d-flex flex-column justify-content-between shadow "
+                className="col-5 col-md-3 plan  shadow h-100  "
                 style={{
                   borderRadius: "1rem",
                   height: "7rem",
@@ -121,7 +121,7 @@ export const Step2 = () => {
                     planPrice: planPrices["advanced"],
                   });
                 }}
-                className="col-3 plan shadow"
+                className="col-5 col-md-3 plan shadow  h-100"
                 style={{ borderRadius: "1rem" }}
               >
                 <img src={advanced} alt="" className="p-3" />
@@ -139,7 +139,7 @@ export const Step2 = () => {
                     planPrice: planPrices["pro"],
                   });
                 }}
-                className="col-3 plan shadow"
+                className="col-5 col-md-3 plan shadow  h-100"
                 style={{
                   borderRadius: "1rem",
                 }}
